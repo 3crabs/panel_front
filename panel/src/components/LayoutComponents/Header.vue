bbf4e7a2f05125c3445f9d108f59877726a26247
old-flex <kostyaniusgaming@gmail.com>
2021-06-30 18:13:40 +0700

    init commit

---
+++ panel/src/components/LayoutComponents/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-header class="bg-white text-black">
+    <q-toolbar>
+      <q-toolbar-title class="text-center main-text">
+          panel
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.main-text {
+  background: linear-gradient(to top, #F46B45, #EEA849);
+  font-family: Inter,sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 28px;
+  letter-spacing: -0.04em;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
---
+++ panel/src/pages/Login.vue
@@ -0,0 +1,85 @@
+<template>
+    <q-layout view="lHh lpr lFf" class="flex content-center justify-center">
+
+      <q-page-container class="flex column content-center justify-center" style="padding-bottom: 70px;">
+        <div class="main-text q-mb-lg">
+          panel`
+        </div>
+
+        <div class="form">
+          <small>логин</small>
+          <q-input class="login q-mb-lg" filled style="border-radius: 6px" v-model="login" :dense="true"/>
+
+          <small>пароль</small>
+          <q-input class="password" type="password" filled style="border-radius: 6px" v-model="password" :dense="true" />
+          <div class="error-message">
+            {{errorMessage}}
+          </div>
+        </div>
+
+        <div class="q-mt-lg">
+          <q-btn class="login-button" unelevated no-caps>
+            войти
+          </q-btn>
+        </div>
+
+
+
+      </q-page-container>
+
+    </q-layout>
+
+</template>
+
+<script>
+import Header from "../components/LayoutComponents/Header";
+export default {
+  name: 'Login',
+  components: {Header},
+
+  data() {
+    return {
+      login: "",
+      password: "",
+      errorMessage: "",
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login{
+  width: 425px;
+}
+small{
+  font-size: 15px;
+  text-align: left;
+  color: #425466;
+}
+.login-button{
+  background: #F2994A;
+  font-family: Inter,sans-serif;
+  font-style: normal;
+  font-size: 15px;
+  line-height: 14px;
+  color: white;
+  width: 425px;
+}
+.error-message{
+  color: #FF5151;
+  height: 12px;
+  text-align: center;
+  margin-top: 5px;
+}
+.main-text {
+  background: linear-gradient(to top, #F46B45, #EEA849);
+  text-align: center;
+  font-family: Inter,sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 125px;
+  letter-spacing: -0.04em;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
